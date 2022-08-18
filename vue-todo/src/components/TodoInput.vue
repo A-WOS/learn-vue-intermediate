cdb0732273af1b68dd73588e066a6a03b0fbdf18
AWOS <uws0331@gmail.com>
2022-08-18 12:36:56 +0900

    style: 전반적인 스타일 수정

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -2,7 +2,7 @@
   <div class="inputBox shadow">
     <!-- v-model: input에 입력된 
     테스트 값을 동적으로 뷰인스턴스 안에 매핑 -->
-    <input type="text" v-model="newTodoItem">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
     <!-- <button v-on:click="addTodo">add</button> -->
     <span class="addContainer" v-on:click="addTodo">
       <i class="fa-solid fa-plus addBtn"></i>
@@ -44,6 +44,8 @@ input:foucs {
   border-radius: 5px;
 }
 .inputBox input {
+  height: 40px;
+  width: 80%;
   border-style: none;
   font-size: 0.9rem;
 }
@@ -56,6 +58,7 @@ input:foucs {
 }
 .addBtn {
   color: white;
+  width: 50px;
   vertical-align: middle;
 }
 </style>
